7696c3434c306d26f35efac17e78590522295b9d
Lucas Corbo <corbo.luca@gmail.com>
2021-11-02 00:47:42 -0300

    https://www.codewars.com/kata/5390bac347d09b7da40006f6/train/java

---
+++ src/jaden_casing_strings/JadenCase.java
@@ -0,0 +1,22 @@
+package jaden_casing_strings;
+
+public class JadenCase {
+	
+	public String toJadenCase(String phrase) {
+	// TODO put your code below this comment
+	    String result = "";
+	    if (phrase == null || phrase.equals("")){
+	    	result = null;
+	    } else {
+	    	String[] words = phrase.split("\\s");
+	    	for (String word: words){
+	    		String firstLetter = word.substring(0,1);
+	    		String rest = word.substring(1);
+	    		result += firstLetter.toUpperCase() + rest + " "; 
+	    	}
+	    	result = result.trim();
+	    }
+		return result;
+	}
+	
+}
